e480dbab9d77584093f59b2a17dc5e9d78b65fab
JonssonJohannae <johanna_jonson@hotmail.com>
2022-10-19 11:43:21 +0200

    Added style components for map

--- components/MapContainer/MapContainer.js
+++ components/MapContainer/MapContainer.js
@@ -1,30 +1,27 @@
-
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import { MapContainerWrapper } from "./styles";
-
+import { MapContainerWrapper, MapContainerText } from "./styles";
 
 const MapContainer = () => {
+  const mapContainerStyle = {
+    width: "50vw",
+    height: "50vh",
+  };
+  const center = { lat: 57.70887, lng: 11.97456 };
 
-    const mapContainerStyle = {
-        width: "50vw",
-        height: "50vh",
-      };
-      const center = { lat: 57.70887, lng: 11.97456 };
-      
-      const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
-    return ( 
+  return (
+    <MapContainerWrapper>
+      <LoadScript googleMapsApiKey={apiKey}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+        ></GoogleMap>
+      </LoadScript>
+      <MapContainerText>Filtrera</MapContainerText>
+    </MapContainerWrapper>
+  );
+};
 
-        <MapContainerWrapper>
-        <LoadScript googleMapsApiKey={apiKey}>
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={8}
-            center={center}
-          ></GoogleMap>
-        </LoadScript>
-      </MapContainerWrapper>
-     );
-}
-
-export default MapContainer; 
+export default MapContainer;
--- components/MapContainer/styles.js
+++ components/MapContainer/styles.js
@@ -1,7 +1,14 @@
-
 import styled from "styled-components";
 
 export const MapContainerWrapper = styled.div`
-height: 50%;
-width: 50%;
+  height: 50%;
+  width: 50%;
+  display: flex;
+  background-color: beige;
+`;
+
+export const MapContainerText = styled.h2`
+  font-weight: bold;
+  padding: 3rem;
+  color: black;
 `;
