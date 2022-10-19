552e7cd7dafcfe976d88f69198adfe4cbcd84d77
JonssonJohannae <johanna_jonson@hotmail.com>
2022-10-19 10:39:21 +0200

    Adjuments
    Adjustments in env file and added api key

--- pages/index.js
+++ pages/index.js
@@ -9,41 +9,26 @@ const mapContainerStyle = {
 };
 const center = { lat: 57.70887, lng: 11.97456 };
 
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
 export default function Home() {
-  // apiKey={"AIzaSyCn18tEMgEvKBitx1sJvGAD9o3J_cnXhJM"}
-  // const { isLoaded, loadError } = useJsApiLoader({
-  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  // });
-  // if (loadError) return "Error with maps";
-  // if (!isLoaded) return "Loading maps";
-
-  return (
-    <div>
-      <LoadScript googleMapsApiKey="AIzaSyBmuxMBOxvnRtJt75AcUvGlFVb3k4vPC10">
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={8}
-          center={center}
-        ></GoogleMap>
-      </LoadScript>
-    </div>
-  );
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Byrårackor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={8}
-          center={center}
-        ></GoogleMap>
-      </div>
       <main className={styles.main}>
-        <h1>Hej </h1>
+        <h1>Byrårackor</h1>
+        <div>
+          <LoadScript googleMapsApiKey={apiKey}>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={8}
+              center={center}
+            ></GoogleMap>
+          </LoadScript>
+        </div>
       </main>
     </div>
   );
