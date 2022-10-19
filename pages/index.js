9b8ec4a008c6eef6cf364398d8759c99628a0dc2
JonssonJohannae <johanna_jonson@hotmail.com>
2022-10-19 09:55:30 +0200

    Map
    Google map works.

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, useJsApiLoader } from "@react-google-maps/api";
 
 const mapContainerStyle = {
   width: "100vw",
   height: "100vh",
-}; 
-const center = { latitude: 57.708870, longitude: 11.974560 };
+};
+const center = { lat: 57.70887, lng: 11.97456 };
 
 export default function Home() {
   // apiKey={"AIzaSyCn18tEMgEvKBitx1sJvGAD9o3J_cnXhJM"}
-  const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey:process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  });
-  if (loadError) return "Error with maps";
-  if (!isLoaded) return "Loading maps";
+  // const { isLoaded, loadError } = useJsApiLoader({
+  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  // });
+  // if (loadError) return "Error with maps";
+  // if (!isLoaded) return "Loading maps";
 
   return (
     <div>
-      <GoogleMap mapContainerStyle={mapContainerStyle} 
-      zoom={8}
-      center={center}></GoogleMap>
+      <LoadScript googleMapsApiKey="AIzaSyBmuxMBOxvnRtJt75AcUvGlFVb3k4vPC10">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+        ></GoogleMap>
+      </LoadScript>
     </div>
   );
 
@@ -31,12 +35,16 @@ export default function Home() {
         <title>Byrårackor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+        ></GoogleMap>
+      </div>
       <main className={styles.main}>
         <h1>Hej </h1>
       </main>
-
-     
     </div>
   );
 }
