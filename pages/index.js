e20db0449617f12e8912105b2ebf996f6223cc21
JonssonJohannae <johanna_jonson@hotmail.com>
2022-10-24 10:26:09 +0200

    Firestore
    Connected with database

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import MapContainer from "../components/MapContainer/MapContainer";
+import { getMarkers } from "../utils/markers";
+import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  // useEffect(() => {
+  //   getMarkers();
+  // }, []);
+  const config = {
+    apiKey: "AIzaSyAqOSxIWs4shc_X0QQpsTSIZnbuAU0ncoc",
+
+    authDomain: "bureau-racks-9c865.firebaseapp.com",
+
+    projectId: "bureau-racks-9c865",
+
+    storageBucket: "bureau-racks-9c865.appspot.com",
+
+    messagingSenderId: "488511466585",
+
+    appId: "1:488511466585:web:9208008771fa8f6eb7d548",
+
+    measurementId: "G-YNKDBPYEGX",
+  };
+  // const firestore = initializeApp(config);
+
+  // export { firestore };
+  initializeApp(config);
+
+  const db = getFirestore();
+
+  const colRef = collection(db, "markers");
+
+  getDocs(colRef).then((snapshot) => {
+    console.log(snapshot.docs);
+  });
   return (
     <div>
       <Head>
