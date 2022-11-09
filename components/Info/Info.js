0612cec5105d31c30524fdaf3ebbabcc5e2e85ac
JonssonJohannae <johanna_jonson@hotmail.com>
2022-11-09 13:12:27 +0100

    Added styling to about page

--- components/Info/Info.js
+++ components/Info/Info.js
@@ -1,8 +1,13 @@
-import { Heading, HeadingWrapper } from "./styles";
+import { AboutText, Heading, HeadingWrapper } from "./styles";
 const Info = () => {
   return (
     <HeadingWrapper>
       <Heading>Om oss</Heading>
+      <AboutText>
+        Här kan du se hundvänliga kontor i Göteborg för dig som vill kunna ta
+        med din hund på jobbet, eller om du hitta ett ställe där det redan finns
+        en gosig kontorskompis.
+      </AboutText>
     </HeadingWrapper>
   );
 };
--- components/Info/styles.js
+++ components/Info/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const HeadingWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-content: center;
 `;
@@ -11,4 +12,13 @@ export const Heading = styled.h1`
   font-size: 5.5rem;
   color: #172d0a;
   padding-right: 1rem;
+  align-self: center;
+`;
+
+export const AboutText = styled.text`
+  font-size: 2rem;
+  color: #172d0a;
+  text-align: center;
+  align-self: center;
+  width: 85%;
 `;
