f1e637013500b3ebd581446adcaaee3d7e284f8b
JonssonJohannae <johanna_jonson@hotmail.com>
2022-11-14 13:42:16 +0100

    update conflict

--- components/Navbar/styles.js
+++ components/Navbar/styles.js
@@ -8,27 +8,12 @@ export const NavbarWrapper = styled.div`
   justify-content: flex-end;
   margin-bottom: 44px;
   font-family: HWT;
-<<<<<<< Updated upstream
-  border-bottom: #172d0a 1.5px solid;
-  @media ${device.tablet} {
-    padding: 2rem 4rem 1rem 4rem;
-  }
-=======
   border-bottom: var(--green) 1.5px solid;
->>>>>>> Stashed changes
 `;
 
 export const NavLink = styled.a`
   padding-left: 3rem;
   cursor: pointer;
-<<<<<<< Updated upstream
-  font-size: 15px;
-  color: #172d0a;
-  @media ${device.tablet} {
-    font-size: 30px;
-  }
-=======
-  font-size:var(--h2-desktop);
-  color:var(--green);
->>>>>>> Stashed changes
+  font-size: var(--h2-desktop);
+  color: var(--green);
 `;
