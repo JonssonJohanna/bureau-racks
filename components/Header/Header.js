ef9b60a85c5d6b936b47a4fd5f541ceed609e53f
Amanda Karlsson Printz <90208099+amandaprintz@users.noreply.github.com>
2022-11-14 10:57:03 +0100

    Styling header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,4 @@
-import { TitleWrapper, Heading  } from "./styles";
+import { TitleWrapper, Heading, HeroText, HeroDescription, HeroSection } from "./styles";
 import walking from './../../public/walking.png'
 import Image from 'next/image'
 
@@ -7,11 +7,22 @@ import Image from 'next/image'
     return (
         <TitleWrapper>
 
-    <Heading>
-        <text>Byrårackor.se</text>
-      </Heading>
-      <Image src={walking} width='150' height='120px' alt='standing dog'/>
-         </TitleWrapper>
+            <HeroSection>
+            <Heading>
+             <text>Byrårackor.se</text>
+             </Heading>
+             <Image src={walking} width='150' height='120px' alt='standing dog'/>
+            </HeroSection>
+            
+            <HeroDescription>
+            <HeroText>
+            Här kan du se hundvänliga kontor i Göteborg för dig som vill kunna ta med din hund på jobbet,
+             eller om du hitta ett ställe där det redan finns en gosig  kontorskompis.
+            </HeroText>
+            </HeroDescription>
+
+        </TitleWrapper>
+
     );
   };
   
--- components/Header/styles.js
+++ components/Header/styles.js
@@ -3,11 +3,17 @@ import styled from "styled-components";
 
 export const TitleWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
   justify-content: center;
-
 `;
+
+export const HeroSection  = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+`
 
 export const Heading = styled.text`
 font-family:"HWT";
@@ -22,4 +28,19 @@ width: 100px;
 
 `;
 
+export const HeroDescription = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
 
+`;
+export const HeroText = styled.text`
+width: 45%;
+font-family: Avenir;
+text-align: center;
+padding-bottom: 1rem;
+`;
+
+export const Intro = styled.div`
+padding-top: 2rem;
+`;
